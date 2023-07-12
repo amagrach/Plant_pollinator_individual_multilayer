0eb725e0821fbfaf5e244330f067c9f803958f8b
Alfonso Allen-Perkins <alfonso.allen@hotmail.com>
2023-07-12 08:30:10 +0200

    Update 00_clean_data_donana_cuadrant_info.R

--- R_scripts/00_clean_data_donana_cuadrant_info.R
+++ R_scripts/00_clean_data_donana_cuadrant_info.R
@@ -11,147 +11,186 @@ library(lubridate)
 # firstly, we get the coord2 data independently, and, then, we use it
 # to replace the damaged data.
 
-fitness_data2_aux <- read_csv("data/donana/cuad_donana_2020_clean.csv")
-fitness_data2_aux$Y <- as.numeric(fitness_data2_aux$Y)
+plant_position_info <- read_csv("data/donana/cuad_donana_20_clean.csv",
+                              col_types = cols_only(
+                                'Bosque'   = col_character(),
+                                'Codigo'   = col_character(),
+                                'Plant_id'  = col_character(),
+                                'Plant' = col_character(),
+                                'quad2' = col_character()),
+                              locale = readr::locale(encoding = "latin1")) %>%
+  separate(quad2,c("X","Y"),",") %>% distinct(.keep_all = TRUE)
 
+plant_position_info$X <- as.numeric(plant_position_info$X)
+plant_position_info$Y <- as.numeric(plant_position_info$Y)
 
-
-x <- fitness_data2_aux %>% group_by(Periodo_hora,Hora) %>% count()
-
-fitness_data2_aux$coord2 <- sub(",", ".", fitness_data2_aux$coord2)
-coord2_2020 <- as.numeric(fitness_data2_aux$coord2)
 
 # Sanity checks
-fitness_data2_aux %>% filter(X>24)
-fitness_data2_aux %>% filter(Y>24)
+plant_position_info %>% filter(X>24)
+plant_position_info %>% filter(Y>24)
+
+# Sequences consist in visiting 2 subplots. Each row refers to a sequence
+# Since there are no coordinates for subplot cc19, we remove it from the dataset
+
+fitness_data2_aux <- read_csv("data/donana/cuad_donana_20_clean.csv") %>%
+  dplyr::select(-quad,-quad2) %>%
+  filter(!Planta.2 %in% c("cc19","RO7"))
+
+fitness_data2_aux$Codigo_vuelo <- 1:nrow(fitness_data2_aux)
+
+fitness_data2 <- NULL
+
+for(i in 1:nrow(fitness_data2_aux)){
+  
+  row_i <- fitness_data2_aux[i,]
+  
+  sequence_i_plant_1 <- row_i %>% dplyr::slice(rep(row_number(), row_i$numero.visitas.1)) %>%
+    dplyr::select(year,Bosque,Codigo,Tanda,Dia,Codigo_vuelo,Pollinator_id,Planta.1) %>%
+    mutate(Plant_id = paste0(Codigo,Planta.1)) %>% dplyr::select(-Planta.1)
+  
+  sequence_i <- sequence_i_plant_1
+  
+  sequence_i$Codigo_within_sequence <- 1:nrow(sequence_i)
+  
+  if(row_i$numero.visitas2 > 1){
+    
+    sequence_i_plant_2 <- row_i %>% dplyr::slice(rep(row_number(), row_i$numero.visitas.1)) %>%
+      dplyr::select(year,Bosque,Codigo,Tanda,Dia,Codigo_vuelo,Pollinator_id,Planta.2) %>%
+      mutate(Plant_id = paste0(Codigo,Planta.2)) %>% dplyr::select(-Planta.2)
+    
+    sequence_i <- bind_rows(sequence_i,sequence_i_plant_2)
+    
+    sequence_i$Codigo_within_sequence <- 1:nrow(sequence_i)
+    
+  }
+  
+  fitness_data2 <- bind_rows(fitness_data2,sequence_i)
+}
 
 
-fitness_data2 <- read_csv("data/gorbea/cuad_gorbea_2020_clean.csv",
-                          locale = readr::locale(encoding = "latin1"))
 
+fitness_data3 <- fitness_data2 %>% 
+  left_join(plant_position_info, by = c("Bosque", "Codigo", "Plant_id")) %>%
+  rename(Year = year, Polinizador = Pollinator_id, Planta = Plant) %>%
+  mutate(Periodo_hora = NA, Fecha = lubridate::dmy(Dia)) %>%
+  dplyr::select(-Codigo,-Dia)
 
-fitness_data2$coord2 <- coord2_2020
-fitness_data2$Y <- as.numeric(fitness_data2$Y)
-
-fitness_data2$Periodo_fecha  %>% unique() %>% sort()
-fitness_data2$Periodo_hora  %>% unique() %>% sort()
-fitness_data2$Bosque  %>% unique() %>% sort()
 
 # Add year info
-fitness_data2$Year <- 2020
-fitness_data2$Day_ISO <- yday(fitness_data2$Fecha)
-fitness_data2$Day <- (fitness_data2$Day_ISO-(min(fitness_data2$Day_ISO)-1))
-fitness_data2$Week_ISO <- strftime(fitness_data2$Fecha, format = "%V")
-fitness_data2$Week <- ceiling((fitness_data2$Day)/7)
+fitness_data3$Day_ISO <- yday(fitness_data3$Fecha)
+fitness_data3$Day <- (fitness_data3$Day_ISO-(min(fitness_data3$Day_ISO)-1))
+fitness_data3$Week_ISO <- strftime(fitness_data3$Fecha, format = "%V")
+fitness_data3$Week <- ceiling((fitness_data3$Day)/7)
 
-fitness_data2$Week %>% unique() %>% sort()
+fitness_data3$Week %>% unique() %>% sort()
 
-# Fix X, Y coordinates
-fitness_data2$X <- round(fitness_data2$coord2,0)
-fitness_data2$Y <- round(100*(fitness_data2$coord2-fitness_data2$X),0)
 
 # Sanity checks
-sum(fitness_data2$X>=0)==nrow(fitness_data2)
-sum(fitness_data2$Y>=0)==nrow(fitness_data2)
-sum(fitness_data2$X<=24)==nrow(fitness_data2)
-sum(fitness_data2$Y<=24)==nrow(fitness_data2)
+sum(fitness_data3$X>=0)==nrow(fitness_data3)
+sum(fitness_data3$Y>=0)==nrow(fitness_data3)
+sum(fitness_data3$X<=24)==nrow(fitness_data3)
+sum(fitness_data3$Y<=24)==nrow(fitness_data3)
 
 # Number of sites without coordinates
 sum(is.na(fitness_data2$X))+sum(is.na(fitness_data2$Y)) # 0
 
 # Review names: Remove dots and spaces
 
-fitness_data2$Planta %>% unique() %>% sort()
-fitness_data2$Polinizador %>% unique() %>% sort()
+fitness_data3$Planta %>% unique() %>% sort()
+fitness_data3$Polinizador %>% unique() %>% sort()
 
 
 # All entries contain spaces
-fitness_data2$Planta[grep(" ",fitness_data2$Planta)] 
-fitness_data2$Polinizador[grep(" ",fitness_data2$Polinizador)]
+fitness_data3$Planta[grep(" ",fitness_data3$Planta)] 
+fitness_data3$Polinizador[grep(" ",fitness_data3$Polinizador)]
 
 # Labels also contain dots
-fitness_data2$Planta[grep("\\.",fitness_data2$Planta)]
-fitness_data2$Polinizador[grep("\\.",fitness_data2$Polinizador)]
+fitness_data3$Planta[grep("\\.",fitness_data3$Planta)]
+fitness_data3$Polinizador[grep("\\.",fitness_data3$Polinizador)]
 
 # Remove spaces from labels
-fitness_data2$Planta <- sub(" ", "_", fitness_data2$Planta)
-fitness_data2$Polinizador <- sub(" ", "_", fitness_data2$Polinizador)
-fitness_data2$Polinizador <- sub(" ", "_", fitness_data2$Polinizador)
+fitness_data3$Planta <- sub(" ", "_", fitness_data3$Planta)
+fitness_data3$Polinizador <- sub(" ", "_", fitness_data3$Polinizador)
+fitness_data3$Polinizador <- sub(" ", "_", fitness_data3$Polinizador)
 
 # Remove dots from labels
-fitness_data2$Planta <- sub("\\.", "", fitness_data2$Planta)
-fitness_data2$Polinizador <- sub("\\.", "", fitness_data2$Polinizador)
+fitness_data3$Planta <- sub("\\.", "", fitness_data3$Planta)
+fitness_data3$Polinizador <- sub("\\.", "", fitness_data3$Polinizador)
 
 #Sanity check
 # All entries contain spaces
-fitness_data2$Planta[grep(" ",fitness_data2$Planta)] 
-fitness_data2$Polinizador[grep(" ",fitness_data2$Polinizador)]
+fitness_data3$Planta[grep(" ",fitness_data3$Planta)] 
+fitness_data3$Polinizador[grep(" ",fitness_data3$Polinizador)]
 
 # Labels also contain dots
-fitness_data2$Planta[grep("\\.",fitness_data2$Planta)]
-fitness_data2$Polinizador[grep("\\.",fitness_data2$Polinizador)]
+fitness_data3$Planta[grep("\\.",fitness_data3$Planta)]
+fitness_data3$Polinizador[grep("\\.",fitness_data3$Polinizador)]
 
 # Recheck labels
-fitness_data2$Planta %>% unique() %>% sort()
-fitness_data2$Polinizador %>% unique() %>% sort()
+fitness_data3$Planta %>% unique() %>% sort()
+fitness_data3$Polinizador %>% unique() %>% sort()
 
 # Fix plant names
-fitness_data2$Planta[grep("Cerastium_sp",fitness_data2$Planta)] <- "Cerastium_vulgare"
-fitness_data2$Planta[grep("Chinopodium_sp",fitness_data2$Planta)] <- "Clinopodium_sp"
-fitness_data2$Planta[grep("Erinus_sp",fitness_data2$Planta)] <- "Clinopodium_sp"
-fitness_data2$Planta[grep("Euphrasia_sp",fitness_data2$Planta)] <- "Euphrasia_alpina"
-fitness_data2$Planta[grep("Galium_sp",fitness_data2$Planta)] <- "Gallium_saxatile"
-fitness_data2$Planta[grep("Gallium_sp",fitness_data2$Planta)] <- "Gallium_saxatile"
-fitness_data2$Planta[grep("Helianthemum_sp",fitness_data2$Planta)] <- "Helianthemum_nummularium"
-fitness_data2$Planta[grep("Linaria_sp",fitness_data2$Planta)] <- "Linaria_propinqua"
-fitness_data2$Planta[grep("Moeringia_trinervia",fitness_data2$Planta)] <- "Moehringia_trinervia"
-fitness_data2$Planta[grep("Myosotis_sp",fitness_data2$Planta)] <- "Myosotis_lamottiana"
-fitness_data2$Planta[grep("Scorzonera_sp",fitness_data2$Planta)] <- "Scorzonera_humilis"
-fitness_data2$Planta[grep("Teucrium_pyrenaica",fitness_data2$Planta)] <- "Teucrium_pyrenaicum"
-fitness_data2$Planta[grep("Thymus_sp",fitness_data2$Planta)] <- "Thymus_praecox"
+fitness_data3$Polinizador[grep("abeja",fitness_data3$Polinizador)] <- "Abeja"
+
 # Save data
-
-fitness_data2_final <- fitness_data2 %>%
+fitness_data3_final <- fitness_data3 %>%
   select(Year,Day_ISO,Week_ISO,Week,
-         Periodo_hora,Bosque,Codigo_vuelo_dia,Codigo_vuelo,
-         Polinizador,Codigo_captura,Planta,X,Y)
+         Periodo_hora,Bosque,Codigo_vuelo,Codigo_within_sequence,
+         Polinizador,Planta,X,Y)
 
-write_csv(fitness_data2_final,"results/gorbea/foraging_Gorbea_2020.csv")
+write_csv(fitness_data3_final,"results/donana/foraging_Donana_2020.csv")
 
 ####################################################
 # DATA FOR 2021
 
-fitness_data21 <- read_csv("data/gorbea/cuad_gorbea_2021_clean.csv") %>% 
-  rename(coord2 = xy) %>%
-  select(-X1) # row number
+fitness_data21_raw <- read_csv("data/donana/cuad_donana_21_clean.csv") %>%
+  mutate(Polinizador = paste0(Pollinator_genus,"_",Pollinator_species)) %>%
+  filter(!is.na(Planta_vistada))
 
-# Exploring the data
-fitness_data21$Periodo_fecha  %>% unique() %>% sort()
-fitness_data21$Periodo_hora  %>% unique() %>% sort()
-fitness_data21$Bosque  %>% unique() %>% sort()
+colnames(fitness_data21_raw)
+
+fitness_data21_aux <- fitness_data21_raw[,-c(15:51)] %>%
+  dplyr::select(-`coord_planta(x)`,-`coord_planta(y)`,
+                -`planta(dentro cuadro)`,-`Codigo-Vuelos`,-Orden_vuelo) %>%
+  rename(X = `coord_flores(x)`, Y = `coord_flores(y)`, 
+         n_visitas = `nº visitas`, Planta = Planta_vistada,
+         Codigo_vuelo = `Codigo-vuelo-dia`,
+         Periodo_hora = Hora)
+
+# Fix n_visitas = NA
+
+fitness_data21_aux$n_visitas[is.na(fitness_data21_aux$n_visitas)] <- 1
+
+
+fitness_data21 <- NULL
+
+for(i in 1:nrow(fitness_data21_aux)){
+  
+  row_i <- fitness_data21_aux[i,]
+  sequence_i <- row_i %>% dplyr::slice(rep(row_number(), row_i$n_visitas))
+  sequence_i$Codigo_within_sequence <- 1:nrow(sequence_i)
+  fitness_data21 <- bind_rows(fitness_data21,sequence_i)
+}
 
 # Add year info
 
 fitness_data21$Year <- 2021
+fitness_data21$Fecha <- lubridate::dmy(fitness_data21$Fecha)
 fitness_data21$Day_ISO <- yday(fitness_data21$Fecha)
 fitness_data21$Day <- (fitness_data21$Day_ISO-(min(fitness_data21$Day_ISO)-1))
 fitness_data21$Week_ISO <- strftime(fitness_data21$Fecha, format = "%V")
 fitness_data21$Week <- ceiling((fitness_data21$Day)/7)
 
-fitness_data21$Week %>% unique() %>% sort()
 
-# Fix X, Y coordinates
-fitness_data21$coord2[fitness_data21$coord2==7.27] <- 7.24
-fitness_data21$Y[fitness_data21$Y=="27"] <- "24"
-fitness_data21$X <- as.numeric(fitness_data21$X)
-fitness_data21$Y <- as.numeric(fitness_data21$Y)
+fitness_data21$Week %>% unique() %>% sort()
+fitness_data21$Periodo_hora %>% unique() %>% sort()
 
 # Sanity checks
 sum(fitness_data21$X>=0)==nrow(fitness_data21)
 sum(fitness_data21$Y>=0)==nrow(fitness_data21)
 sum(fitness_data21$X<=24)==nrow(fitness_data21)
 sum(fitness_data21$Y<=24)==nrow(fitness_data21)
-
 
 # Number of sites without coordinates
 sum(is.na(fitness_data21$X))+sum(is.na(fitness_data21$Y))
@@ -160,16 +199,16 @@ sum(is.na(fitness_data21$X))+sum(is.na(fitness_data21$Y))
 
 fitness_data21$Planta %>% unique() %>% sort()
 fitness_data21$Polinizador %>% unique() %>% sort()
-fitness_data21$Planta[grep("Thymus praecox subsp. polytrichus",fitness_data21$Planta)] <- "Thymus praecox"
-fitness_data21$Planta[grep("Pedicularis sylvatica subsp. sylvatica",fitness_data21$Planta)] <- "Pedicularis sylvatica"
 
-
-fitness_data21$Polinizador[grep("Andrena cf. Morio",fitness_data21$Polinizador)] <- "Andrena cf morio"
-fitness_data21$Polinizador[grep("?=80",fitness_data21$Polinizador)] <- "?=C80"
-fitness_data21$Polinizador[grep("?=81",fitness_data21$Polinizador)] <- "?=C81"
-
-
-
+# Fix polinator names
+fitness_data21$Polinizador[grep("Andrea_flavipes",fitness_data21$Polinizador)] <- "Andrena_flavipes"
+fitness_data21$Polinizador[grep("Anthophora_NA",fitness_data21$Polinizador)] <- "Anthophora_sp"
+fitness_data21$Polinizador[grep("Empis_NA",fitness_data21$Polinizador)] <- "Empis_sp"
+fitness_data21$Polinizador[grep("Episyrphus_NA",fitness_data21$Polinizador)] <- "Episyrphus_sp"
+fitness_data21$Polinizador[grep("Eucera_NA",fitness_data21$Polinizador)] <- "Eucera_sp"
+fitness_data21$Polinizador[grep("Hymenoptera_NA",fitness_data21$Polinizador)] <- "Hymenoptera_sp"
+fitness_data21$Polinizador[grep("Lasioglossum_NA",fitness_data21$Polinizador)] <- "Lasioglossum_sp"
+fitness_data21$Polinizador[grep("Ucera_NA",fitness_data21$Polinizador)] <- "Eucera_sp"
 
 # All entries contain spaces
 fitness_data21$Planta[grep(" ",fitness_data21$Planta)] 
@@ -194,31 +233,14 @@ fitness_data21$Polinizador <- sub("\\.", "", fitness_data21$Polinizador)
 fitness_data21$Planta[grep(" ",fitness_data21$Planta)] 
 fitness_data21$Polinizador[grep(" ",fitness_data21$Polinizador)]
 
-
-# Fix plant names
-fitness_data21$Planta[grep("Cerastium_sp",fitness_data21$Planta)] <- "Cerastium_vulgare"
-fitness_data21$Planta[grep("Chinopodium_sp",fitness_data21$Planta)] <- "Clinopodium_sp"
-fitness_data21$Planta[grep("Erinus_sp",fitness_data21$Planta)] <- "Clinopodium_sp"
-fitness_data21$Planta[grep("Euphrasia_sp",fitness_data21$Planta)] <- "Euphrasia_alpina"
-fitness_data21$Planta[grep("Galium_sp",fitness_data21$Planta)] <- "Gallium_saxatile"
-fitness_data21$Planta[grep("Gallium_sp",fitness_data21$Planta)] <- "Gallium_saxatile"
-fitness_data21$Planta[grep("Helianthemum_sp",fitness_data21$Planta)] <- "Helianthemum_nummularium"
-fitness_data21$Planta[grep("Linaria_sp",fitness_data21$Planta)] <- "Linaria_propinqua"
-fitness_data21$Planta[grep("Moeringia_trinervia",fitness_data21$Planta)] <- "Moehringia_trinervia"
-fitness_data21$Planta[grep("Myosotis_sp",fitness_data21$Planta)] <- "Myosotis_lamottiana"
-fitness_data21$Planta[grep("Scorzonera_sp",fitness_data21$Planta)] <- "Scorzonera_humilis"
-fitness_data21$Planta[grep("Teucrium_pyrenaica",fitness_data21$Planta)] <- "Teucrium_pyrenaicum"
-fitness_data21$Planta[grep("Thymus_sp",fitness_data21$Planta)] <- "Thymus_praecox"
-
 # Recheck labels
 fitness_data21$Planta %>% unique() %>% sort()
 fitness_data21$Polinizador %>% unique() %>% sort()
 
 fitness_data21_final <- fitness_data21 %>%
   select(Year,Day_ISO,Week_ISO,Week,
-         Periodo_hora,Bosque,Codigo_vuelo_dia,Codigo_vuelos,
-         Polinizador,Codigo_captura,Planta,X,Y) %>%
-  rename(Codigo_vuelo = Codigo_vuelos)
+         Periodo_hora,Bosque,Codigo_vuelo,Codigo_within_sequence,
+         Polinizador,Planta,X,Y)
 
 write_csv(fitness_data21_final,"results/gorbea/foraging_Gorbea_2021.csv")
 
