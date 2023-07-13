af5c3c7aacbce8827c9e5ac37d5e2c4eb40f1c99
Alfonso Allen-Perkins <alfonso.allen@hotmail.com>
2023-07-13 09:36:43 +0200

    Update 04_model_fit_donana_step_probability.R

--- R_scripts/04_model_fit_donana_step_probability.R
+++ R_scripts/04_model_fit_donana_step_probability.R
@@ -13,7 +13,7 @@ library(survival)
 library(car)
 
 source("R_scripts/aux_functions/pollinator_model_exp_coef.R")
-source("R_scripts/aux_functions/set_same_factor_levels_no_flower_data.R")
+source("R_scripts/aux_functions/set_same_factor_levels_on_flower_data.R")
 
 number_random_steps <- 5
 
@@ -37,7 +37,7 @@ for(pollinator_i in ranking_pollinators$Polinizador[1:5]) {
   
   pollinator_i_data_clogit_within_field <- pollinator_i_data_clogit
   
-  pollinator_i_data_clogit_mod <- set_same_factor_levels_no_flower_data(pollinator_i_data_clogit_within_field) %>%
+  pollinator_i_data_clogit_mod <- set_same_factor_levels_on_flower_data_donana(pollinator_i_data_clogit_within_field) %>%
     mutate(cosine_turning = cos(turning_angle*pi/180))
   
   pollinator_i_data_clogit_mod$Periodo <- as.factor(pollinator_i_data_clogit_mod$Periodo)
@@ -49,8 +49,8 @@ for(pollinator_i in ranking_pollinators$Polinizador[1:5]) {
   
   model_pollinator_i <- clogit(control ~ step_length + #time_of_day +
                                  step_length : time_of_day +
-                                 step_length : plot + 
-                                 step_length : Year + 
+                                 # step_length : plot + 
+                                 # step_length : Year + 
                                  step_length : change_plant_sp +
                                  # step_length : Periodo +
                                  delta_richness +
